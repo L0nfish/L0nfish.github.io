d5f6a97e853e6ed7793f7070fa65edba8a5fa783
L0nfish <66016225+L0nfish@users.noreply.github.com>
2020-12-09 22:11:09 +0600

    Add files via upload

---
+++ textanima.js
@@ -0,0 +1,14 @@
+
+        $('#credo2').typeIt({
+            speed: 110,
+            loop:true,
+        })
+        .tiType("My picture - your history")
+        .tiPause(700)
+        .tiDelete(7)
+        .tiType('future')
+        .tiPause(700)
+        .tiDelete(6)
+        .tiType('smile)')
+        .tiPause(1000)
+        .tiGo();
